e2f8d7e1e4776d1f5da84321420d3075f0e14f4a
JMPinillos <pinillosrubio@gmail.com>
2025-07-28 15:02:21 +0200

    update

---
+++ hola_mundo/workspace/curso-py/tipos/01-variables.py
@@ -0,0 +1,10 @@
+nombre_curso = "Ultimare Python"
+nombre1 = "Hola"
+NOMBRE_CURSO = "Mundo"
+NombreCurso = "Feliz"
+
+print(nombre_curso, nombre1, NOMBRE_CURSO)
+
+alumnos = 5000
+puntuaje = 0.9
+publicado = True
---
+++ hola_mundo/workspace/curso-py/tipos/03-format-strings.py
@@ -0,0 +1,7 @@
+nombre = "Nicolas"
+apellido = "Schurmann"
+
+# nombre_completo = f"{nombre} {apellido}"
+nombre_completo = f"{nombre[0]} {2+5}"
+
+print(nombre_completo)
