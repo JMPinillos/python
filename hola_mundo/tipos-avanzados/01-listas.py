9c7c1d6b7d403a6aadcf7060bcb19daa2f3698d1
JMPinillos <pinillosrubio@gmail.com>
2025-08-04 15:10:14 +0200

    update

---
+++ hola_mundo/tipos-avanzados/01-listas.py
@@ -0,0 +1,12 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+ceros = [0]*10
+rango = list(range(10))
+rango2 = list(range(1, 11))
+chars = list("Hola Mundo")
+
+print(numeros, letras)
+print(ceros)
+print(rango)
+print(rango2)
+print(chars)
---
+++ hola_mundo/tipos-avanzados/02-manipulando-listas.py
@@ -0,0 +1,14 @@
+mascotas = ["Pachu", "Frida", "Lish"]
+
+print(mascotas)
+print(mascotas[1])
+
+mascotas[1] = "Pulgas"
+
+print(mascotas)
+print(mascotas[0:2])
+print(mascotas[-1])
+print(mascotas[1::2])
+
+numeros = list(range(1, 22))
+print(numeros[0::2])
