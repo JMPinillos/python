4ed7b8399020ffd25d42aff8161878e6caa5297e
JMPinillos <pinillosrubio@gmail.com>
2025-08-05 09:27:41 +0200

    Update 05-agregar-eliminar.py

--- hola_mundo/tipos-avanzados/05-agregar-eliminar.py
+++ hola_mundo/tipos-avanzados/05-agregar-eliminar.py
@@ -9,10 +9,26 @@ mascotas.append("Lupita")
 print(mascotas)
 
 mascotas.append("Frida")
+mascotas.append("Frida")
 print(mascotas)
 
 mascotas.remove("Frida")
 print(mascotas)
 
-for m["Frida"] in mascotas:
-    mascotas.remove(m)
+mascotas = [mascota for mascota in mascotas if mascota != "Frida"]
+print(mascotas)
+
+mascotas.append("Frida")
+print(mascotas)
+
+mascotas.pop()
+print(mascotas)
+
+mascotas.pop(2)
+print(mascotas)
+
+del mascotas[1]
+print(mascotas)
+
+mascotas.clear()
+print(mascotas)
