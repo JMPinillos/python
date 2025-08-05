d7bbece5074e158ef762ce3dc36477a6eb8172bb
JMPinillos <pinillosrubio@gmail.com>
2025-08-05 09:20:34 +0200

    Update 05-agregar-eliminar.py

--- hola_mundo/tipos-avanzados/05-agregar-eliminar.py
+++ hola_mundo/tipos-avanzados/05-agregar-eliminar.py
@@ -4,3 +4,15 @@ print(mascotas)
 
 mascotas.insert(1, "WiFi")
 print(mascotas)
+
+mascotas.append("Lupita")
+print(mascotas)
+
+mascotas.append("Frida")
+print(mascotas)
+
+mascotas.remove("Frida")
+print(mascotas)
+
+for m["Frida"] in mascotas:
+    mascotas.remove(m)
