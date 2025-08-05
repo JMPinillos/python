a19e511361851bb412d5d3d56c92a9ef89eeb2c7
JMPinillos <pinillosrubio@gmail.com>
2025-08-05 15:17:40 +0200

    Update 08-comprension-lista.py

--- hola_mundo/tipos-avanzados/08-comprension-lista.py
+++ hola_mundo/tipos-avanzados/08-comprension-lista.py
@@ -25,6 +25,19 @@ print(usuarios)
 # print(nombres)
 
 # ********** versión 3 con modificación y filtrado *********
-nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
+# nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
 
+# print(nombres)
+
+# ********** Map y Filter *********
+# map haría esto:
+# nombres = [usuario[0] for usuario in usuarios]
+
+# nombres = list(map(lambda usuario: usuario[0], usuarios))
+# print(nombres)
+
+# filter haría esto:
+# nombres = [usuario for usuario in usuarios if usuario[1] > 2]
+
+nombres = list(filter(lambda usuario: usuario[1] > 2, usuarios))
 print(nombres)
