614ce37be591f01bc54e1c971732a8a429c71d00
JMPinillos <pinillosrubio@gmail.com>
2025-08-05 09:07:32 +0200

    Update 04-iterar-listas.py

--- hola_mundo/tipos-avanzados/04-iterar-listas.py
+++ hola_mundo/tipos-avanzados/04-iterar-listas.py
@@ -1,10 +1,11 @@
-mascotas = "Lish", "Pachu", "Frida", "Pulgas", "Lish"
+mascotas = "Lish", "Pachu", "Frida", "Pulgas", "lish"
 
 # for mascota in enumerate(mascotas):
 #     print(mascota[1])
 
 # for indice, mascota in enumerate(mascotas):
 #     print(indice, mascota)
+print(mascotas.count("Lish"))
 
 if "Lish" in mascotas:
-    print(mascotas.count("Lish"))
+    print(mascotas.index("Lish"))
