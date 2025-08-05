31a218e0c35ea1e937cefcfacffbd260abc7713a
JMPinillos <pinillosrubio@gmail.com>
2025-08-05 09:38:29 +0200

    update

--- hola_mundo/tipos-avanzados/03-desempaquetar-listas.py
+++ hola_mundo/tipos-avanzados/03-desempaquetar-listas.py
@@ -1,7 +1,8 @@
 # numeros = [1, 2, 3]
-numeros = range(1, 10)
+numeros = list(range(1, 10))
 # primero, segundo, tercero = numeros
 # primero, *otros = numeros
+
 primero, *otros, ultimo = numeros
 
 print(primero, otros, ultimo)
---
+++ hola_mundo/tipos-avanzados/06-ordenando-listas.py
@@ -0,0 +1,10 @@
+import random
+
+mascotas = ["Lish", "Pachu", "Frida", "Pulgas"]
+numeros_aleatorios = []
+
+for i in range(11):
+    numeros_aleatorios.append(random.randint(1, 100))
+
+print(mascotas)
+print(numeros_aleatorios)
