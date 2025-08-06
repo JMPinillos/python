58781d95922a3551f5dc75963e03d785c3733721
JMPinillos <pinillosrubio@gmail.com>
2025-08-06 13:30:29 +0200

    Update Ejercicio.py

--- hola_mundo/workspace/curso-py/tipos-avanzados/Ejercicio.py
+++ hola_mundo/workspace/curso-py/tipos-avanzados/Ejercicio.py
@@ -1,4 +1,7 @@
+from collections import Counter
 texto = "Hola mundo este es mi string"
+
+# ********** Parte 1 *********
 lista = []
 
 
@@ -7,4 +10,42 @@ def text2list(text):
 
 
 lista = text2list(texto)
-print(lista)
+# print(lista)
+
+# ********** Parte 2 *********
+# ********** Con una función *********
+diccionario = {}
+
+
+def dic_count(text):
+    for char in text:
+        if char in diccionario:
+            diccionario[char] += 1
+        else:
+            diccionario[char] = 1
+
+
+dic_count(lista)
+print(diccionario)
+
+# ********** Con una librería *********
+# diccionario2 = Counter(texto.replace(" ", ""))
+# print(diccionario2)
+
+# ********** Parte 3 *********
+lista_tuplas = []
+
+
+def dic2tuplas(diccionario):
+    lista = []
+    diccionario_ordenado = sorted(
+        diccionario.items(), key=lambda item: item[1], reverse=True)
+
+    for char, count in diccionario_ordenado:
+        lista.append((char, count))
+
+    return lista
+
+
+lista_tuplas = dic2tuplas(diccionario)
+print(lista_tuplas)
