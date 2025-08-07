f641ad921dea710c565598259fa9c347a8c5bb26
JMPinillos <pinillosrubio@gmail.com>
2025-08-07 15:03:29 +0200

    Update 06-privados.py

--- hola_mundo/workspace/clases/06-privados.py
+++ hola_mundo/workspace/clases/06-privados.py
@@ -12,6 +12,12 @@ class Perro:
     def get_nombre(self):
         return self.__nombre
 
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def __metodo_privado(self):
+        print("Accediste a un método privado")
+
     @classmethod
     def factory(cls):
         return cls("Pulgas", 23)
@@ -21,3 +27,12 @@ perro1 = Perro.factory()
 perro1.habla()
 
 print(perro1.get_nombre())
+
+perro1.set_nombre("frida")
+print(perro1.get_nombre())
+
+print(perro1.__dict__)
+
+print(perro1._Perro__nombre)
+
+perro1._Perro__metodo_privado()
