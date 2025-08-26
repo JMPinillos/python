0b476171cfa606c38415ad78ecbc0464fb0105af
JMPinillos <pinillosrubio@gmail.com>
2025-08-26 11:57:48 +0200

    update

---
+++ hola_mundo_2/sqlite/04-insert.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+with sqlite3.connect("sqlite/app.db") as con:
+    cursor = con.cursor()
+
+    cursor.execute(
+        "INSERT INTO usuarios VALUES (?, ?)",
+        (1, "Hola Mundo")
+    )
